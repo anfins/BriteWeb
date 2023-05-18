d0a04625ec265ca2fa5f4c97759208affc0e82a2
Sierra Jackson <sierranj03@gmail.com>
2023-05-18 11:04:18 -0400

    added scroll transitions

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
   
-const observer = new IntersectionObserver((entries) => { //items given the hidden class will appear when seen on screen for the first time and stay on the screen until reload
+const observerLeft = new IntersectionObserver((entries) => { // items given the hidden class will appear when seen on screen for the first time and stay on the screen until reload
 
     entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add("show"); //adds transition
-
+        if(entry.isIntersecting) {
+            entry.target.classList.add("show-left"); // adds transition
         } 
     });
 });
 
-const observerB = new IntersectionObserver((entries) => { //items given the hidden-return class will appear on screen when seen, but disappear again when the user scrolls past their instance 
+const observerRight = new IntersectionObserver((entries) => { // items given the hidden class will appear when seen on screen for the first time and stay on the screen until reload
+
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("show-right"); // adds transition
+        } 
+    });
+});
+
+const observerB = new IntersectionObserver((entries) => { // items given the hidden-return class will appear on screen when seen, but disappear again when the user scrolls past their instance 
 
     entries.forEach((entry) => {
         console.log(entry)
         if(entry.isIntersecting){
-            entry.target.classList.add("show-return"); //adding item to the html
+            entry.target.classList.add("show-return"); // adding item to the html
 
         } 
         else{
-            entry.target.classList.remove("show-return"); //removing item from view
+            entry.target.classList.remove("show-return"); // removing item from view
         }
     });
 });
 
 
-const observerC = new IntersectionObserver((entries) => { //simply a longer transition
+const observerC = new IntersectionObserver((entries) => { // simply a longer transition
 
     entries.forEach((entry) => {
         console.log(entry)
@@ -50,14 +57,15 @@ const observerD = new IntersectionObserver((entries) => { //simply a longer tran
 });
 
 
-const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenLeftElements = document.querySelectorAll(".hidden-left");
+const hiddenRightElements = document.querySelectorAll(".hidden-right");
 const hiddenElementsReturn = document.querySelectorAll(".hidden-return");
 const hiddenElementsSlower = document.querySelectorAll(".hidden-slower");
 const hiddenElementsFly = document.querySelectorAll(".hidden-fly");
 
 
-
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenLeftElements.forEach((el) => observerLeft.observe(el));
+hiddenRightElements.forEach((el) => observerRight.observe(el));
 hiddenElementsReturn.forEach((el) => observerB.observe(el));
 hiddenElementsSlower.forEach((el) => observerC.observe(el));
 hiddenElementsFly.forEach((el) => observerD.observe(el));
